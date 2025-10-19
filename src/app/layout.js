b4326a01bc5470e329c35a88818f0dccf65e1e41
Shanu S <shanuflash@gmail.com>
2025-10-19 14:27:16 +0530

    chore: remove @vercel/speed-insights dependency and related component from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from 'next/font/google';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/next';
 import './globals.css';
 
@@ -78,7 +77,6 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <SpeedInsights />
         <Analytics />
       </body>
     </html>
