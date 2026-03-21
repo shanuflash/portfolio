543cba55ba50b532a2cb4f1c3dfde1c364c61636
Shanu S <shanuflash@gmail.com>
2026-03-21 22:16:53 +0530

    page switch transition

---
+++ src/app/template.js
@@ -0,0 +1,16 @@
+"use client";
+
+import { motion } from "motion/react";
+
+export default function Template({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 8 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -8 }}
+      transition={{ duration: 0.3, ease: [0.25, 0.1, 0.25, 1] }}
+    >
+      {children}
+    </motion.div>
+  );
+}
