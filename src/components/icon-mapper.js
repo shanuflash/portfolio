9d080757ec9f5d6b8f1c1fc8a0dfa0d8003b7ea4
Shanu S <shanuflash@gmail.com>
2026-05-21 19:19:51 +0530

    feat: change project descriptions and change icon for X

--- src/components/icon-mapper.js
+++ src/components/icon-mapper.js
@@ -34,6 +34,14 @@ import {
   MapPin,
 } from 'lucide-react';
 
+function XTwitter({ className }) {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+      <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.253 5.622 5.91-5.622Zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+    </svg>
+  );
+}
+
 const iconMap = {
   Type,
   FileText,
@@ -56,6 +64,7 @@ const iconMap = {
   BarChart3,
   Github,
   Twitter,
+  XTwitter,
   Linkedin,
   Instagram,
   Code,
